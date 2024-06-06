7f206d3e4529c550a308e47a8e4b5e1e4a4cf6f2
Luciano Alves <lasbr_alves@outlook.com>
2024-06-06 15:58:13 -0300

    Project started

---
+++ src/main/java/br/com/lasbr/book/BookNetworkApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.lasbr.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkApiApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/lasbr/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lasbr.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
