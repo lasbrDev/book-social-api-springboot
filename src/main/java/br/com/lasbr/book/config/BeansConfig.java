c82e6f2638c4d13311130ba3272c6052991a5c92
Luciano Alves <lasbr_alves@outlook.com>
2024-06-22 15:23:17 -0300

    feat: added ApplicationAuditAware.

--- src/main/java/br/com/lasbr/book/config/BeansConfig.java
+++ src/main/java/br/com/lasbr/book/config/BeansConfig.java
@@ -3,6 +3,7 @@ package br.com.lasbr.book.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -33,6 +34,11 @@ public class BeansConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
     }
 }
 
