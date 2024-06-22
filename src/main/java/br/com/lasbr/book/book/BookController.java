b8a60de37a52a41cea9b1c8e385a08afba6b449d
Luciano Alves <lasbr_alves@outlook.com>
2024-06-22 20:42:14 -0300

    feat: added BookResponse, also implemented method findBookById.

--- src/main/java/br/com/lasbr/book/book/BookController.java
+++ src/main/java/br/com/lasbr/book/book/BookController.java
@@ -5,10 +5,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
@@ -21,5 +18,10 @@ public class BookController {
     @PostMapping
     public ResponseEntity<Integer> saveBook(@Valid @RequestBody BookRequest request, Authentication connectedUser) {
         return  ResponseEntity.ok(service.save(request, connectedUser));
+    }
+
+    @GetMapping("{book-id}")
+    public ResponseEntity<BookResponse> findBookById(@PathVariable("book-id") Integer bookId) {
+        return ResponseEntity.ok(service.findById(bookId));
     }
 }
--- src/main/java/br/com/lasbr/book/book/BookService.java
+++ src/main/java/br/com/lasbr/book/book/BookService.java
@@ -1,6 +1,7 @@
 package br.com.lasbr.book.book;
 
 import br.com.lasbr.book.user.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -17,5 +18,11 @@ public class BookService {
         Book book = bookMapper.toBook(request);
         book.setOwner(user);
         return bookRepository.save(book).getId();
+    }
+
+    public BookResponse findById(Integer bookId) {
+        return bookRepository.findById(bookId)
+                .map(bookMapper::toBookResponse)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with the ID: " + bookId));
     }
 }
