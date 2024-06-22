4421b6d9aeaad4c564b8d1500622eea6eaf36d41
Luciano Alves <lasbr_alves@outlook.com>
2024-06-22 15:12:31 -0300

    Update .gitignore and remove ignored files from version control

--- src/main/java/br/com/lasbr/book/user/User.java
+++ src/main/java/br/com/lasbr/book/user/User.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.security.Principal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
